8fa9a1633c9fb46ca6794b93ca66739e31138882
daliange <778425935@qq.com>
2017-02-08 22:15:07 +0800

    注解扫描annotation

---
+++ spring/src/main/java/org/spring/annotation/CustomerService.java
@@ -0,0 +1,19 @@
+package org.spring.annotation;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service
+@Scope("prototype")
+public class CustomerService 
+{
+	String message;
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
