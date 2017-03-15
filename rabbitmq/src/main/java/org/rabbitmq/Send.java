5f6130d77ab7ff6e12ada0b86b2aac112988d812
daliange <778425935@qq.com>
2017-03-15 21:59:45 +0800

    rabbitmq hello world

--- rabbitmq/src/main/java/org/rabbitmq/Send.java
+++ rabbitmq/src/main/java/org/rabbitmq/Send.java
@@ -12,17 +12,25 @@ public class Send {
 	private final static String QUEUE_NAME = "hello";
 
 	public static void main(String[] args) throws IOException, TimeoutException {
-		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
-		Connection connection = factory.newConnection();
-		Channel channel = connection.createChannel();
-
-		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-		String message = "Hello World!";
-		channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-		System.out.println(" [x] Sent '" + message + "'");
-
-		channel.close();
-		connection.close();
+        /** 
+         * 创建连接连接到MabbitMQ 
+         */  
+        ConnectionFactory factory = new ConnectionFactory();  
+        //设置MabbitMQ所在主机ip或者主机名  
+        factory.setHost("localhost");  
+        //创建一个连接  
+        Connection connection = factory.newConnection();  
+        //创建一个频道  
+        Channel channel = connection.createChannel();  
+        //指定一个队列  
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);  
+        //发送的消息  
+        String message = "hello world!";  
+        //往队列中发出一条消息  
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());  
+        System.out.println(" [x] Sent '" + message + "'");  
+        //关闭频道和连接  
+        channel.close();  
+        connection.close(); 
 	}
 }
